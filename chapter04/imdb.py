751dcf6c4ca69eb95f654860b4a7d96a9e50c0b4
Marc Altmann <marcaltmann@posteo.de>
2024-02-20 19:13:51 +0100

    Finish first example

--- chapter04/imdb.py
+++ chapter04/imdb.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import layers
 from tensorflow.keras.datasets import imdb
 import numpy as np
 
@@ -9,21 +12,84 @@ def vectorize_sequences(sequences, dimension=10000):
     return results
 
 
-def review(index):
+def review(data_point):
     word_index = imdb.get_word_index()
     reverse_word_index = dict(
         [(value, key) for (key, value) in word_index.items()])
 
     decoded_review = " ".join(
-        [reverse_word_index.get(i - 3, "?") for i in train_data[index]])
+        [reverse_word_index.get(i - 3, "?") for i in data_point])
     return decoded_review
 
 
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(
     num_words = 10000)
 
+
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 
+
 y_train = np.asarray(train_labels).astype("float32")
 y_test = np.asarray(test_labels).astype("float32")
+
+
+model = keras.Sequential([
+    layers.Dense(32, activation="tanh"),
+    layers.Dense(32, activation="tanh"),
+    layers.Dense(1, activation="sigmoid")
+])
+
+
+model.compile(optimizer="rmsprop",
+              loss="mse",
+              metrics=["accuracy"])
+
+
+model.fit(x_train,
+          y_train,
+          epochs=4,
+          batch_size=512)
+
+
+results = model.evaluate(x_test, y_test)
+print(results)
+
+
+predictions = model.predict(x_test)
+
+
+# x_val = x_train[:10000]
+# partial_x_train = x_train[10000:]
+# y_val = y_train[:10000]
+# partial_y_train = y_train[10000:]
+
+# history = model.fit(partial_x_train,
+#                     partial_y_train,
+#                     epochs=20,
+#                     batch_size=512,
+#                     validation_data=(x_val, y_val))
+
+
+# history_dict = history.history
+# loss_values = history_dict["loss"]
+# val_loss_values = history_dict["val_loss"]
+# epochs = range(1, len(loss_values) + 1)
+# plt.plot(epochs, loss_values, "bo", label="Training loss")
+# plt.plot(epochs, val_loss_values, "b", label="Validation loss")
+# plt.title("Training and validation loss")
+# plt.xlabel("Epochs")
+# plt.ylabel("Loss")
+# plt.legend()
+# plt.show()
+
+# plt.clf()
+# acc = history_dict["accuracy"]
+# val_acc = history_dict["val_accuracy"]
+# plt.plot(epochs, acc, "bo", label="Training acc")
+# plt.plot(epochs, val_acc, "b", label="Validation acc")
+# plt.title("Training and validation accuracy")
+# plt.xlabel("Epochs")
+# plt.ylabel("Accuracy")
+# plt.legend()
+# plt.show()
